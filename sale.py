5cb5290eb012972cf70e3eb60495599ac69b0a37
jmartin <jmartin@zikzakmedia.com>
2016-02-24 17:40:28 +0100

    Improve performance on creating shipment since sale

--- sale.py
+++ sale.py
@@ -1,7 +1,7 @@
 # This file is part of the carrier_send_shipments module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
 
 __all__ = ['Sale']
 __metaclass__ = PoolMeta
@@ -10,31 +10,18 @@ __metaclass__ = PoolMeta
 class Sale:
     __name__ = 'sale.sale'
 
-    def create_shipment(self, shipment_type):
-        ShipmentOut = Pool().get('stock.shipment.out')
+    def _get_shipment_sale(self, Shipment, key):
+        shipment = super(Sale, self)._get_shipment_sale(Shipment, key)
+        if Shipment.__name__ == 'stock.shipment.out':
+            if self.carrier and self.carrier.service:
+                shipment.carrier_service = self.carrier.service
 
-        shipments = super(Sale, self).create_shipment(shipment_type)
-        if not shipments:
-            return
+            address = shipment.customer.address_get(type='delivery')
+            if address.comment_shipment:
+                shipment.carrier_notes = shipment._comment2txt(
+                        address.comment_shipment)
+            elif shipment.customer.comment_shipment:
+                shipment.carrier_notes = shipment._comment2txt(
+                        shipment.customer.comment_shipment)
 
-        if shipment_type == 'out':
-            to_save = []
-            for shipment in shipments:
-                if self.carrier and self.carrier.service:
-                    shipment.carrier_service = self.carrier.service
-
-                address = shipment.customer.address_get(type='delivery')
-                if address.comment_shipment:
-                    shipment.carrier_notes = shipment._comment2txt(
-                            address.comment_shipment)
-                elif shipment.customer.comment_shipment:
-                    shipment.carrier_notes = shipment._comment2txt(
-                            shipment.customer.comment_shipment)
-                
-                if shipment.carrier_service or shipment.carrier_notes:
-                    to_save.append(shipment)
-
-            if to_save:
-                ShipmentOut.save(to_save)
-
-        return shipments
+        return shipment
